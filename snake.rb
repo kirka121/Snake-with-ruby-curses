5fe63232b731186bb49a72ead894bbf787c28294
Kirill Afan <kirill.afan@me.com>
2012-07-29 15:07:39 -0400

    added inheritance, fixed some variable scopes

--- snake.rb
+++ snake.rb
@@ -13,12 +13,13 @@ class Snake
 		@game_speed = 0.2
 		@game_score = 0
 		@display_speed = 0
+		@game_score = 0
 		make_food(@height,@width)
 	end
 
 	def check_wall_collision
 		#check collision with border
-		if @pos_y[0] == cols-1 or @pos_y[0] == 0 or @pos_x[0] == lines-1 or @pos_x[0] == 0
+		if @pos_y[0] == @width-1 or @pos_y[0] == 0 or @pos_x[0] == @height-1 or @pos_x[0] == 0
 			GamePlay::end_of_game
 		end
 	end
@@ -32,13 +33,15 @@ class Snake
 		end
 	end
 
-	def check_food_eaten
+	def check_food_eaten(time_offset)
 		#check if ate food
 		if @pos_y[0] == @food_y and @pos_x[0] == @food_x
 			make_food(@height,@width)
 			@snake_len += 1
 			@game_score += 1*@display_speed
 		end
+		setpos(height-1,width-12)
+		addstr("Score: " + (@game_score-(time_offset)/10.round(0)).to_s)
 	end
 
 	def draw_snake
@@ -54,6 +57,8 @@ class Snake
 			setpos(@pos_x[t],@pos_y[t])
 			addstr(t == 1 ? "*" : "+")
 		end
+		setpos(0,3)
+		addstr("Snake Length: " + @snake_len.to_s)
 	end
 
 	def make_food(max_h, max_w)
@@ -62,12 +67,11 @@ class Snake
 	end
 end
 
-class GamePlay
+class GamePlay < Snake
 	def initialize
 		@dir = :right
 		@pause = false
 		@speed_incremented = false
-		@game_speed = 0.2
 	end
 
 	def change_direction_detect
@@ -110,15 +114,17 @@ class GamePlay
 
 	def speed_of_game(time_offset)
 		#set speed of play, increment it automatically
-		if ((snake_len % 10 == 0) or (time_offset%60 == 0))
+		if ((@snake_len % 10 == 0) or (time_offset%60 == 0))
 			if @speed_incremented == false
 				@game_speed -= (@game_speed*0.10) unless @game_speed < 0.05
 				@speed_incremented = true
-				display_speed += 1
+				@display_speed += 1
 			end
 		else
 			@speed_incremented = false
 		end
+		setpos(height-1,3)
+		addstr("Speed: " + @display_speed.to_s)
 	end
 end
 
@@ -135,7 +141,6 @@ title = "Kirka's Snake"
 start_time = Time.now.to_i
 speed_incremented = false
 display_speed = 0
-game_score = 0
 win = Window.new(lines, cols, 0, 0) #set the playfield the size of current terminal window
 
 begin
@@ -150,20 +155,11 @@ begin
 		win.setpos(@food_x, @food_y)
 		win.addstr("#")
 
-		win.setpos(0,3)
-		win.addstr("Snake Length: " + snake_len.to_s)
-
 		win.setpos(0,width/2-title.length/2)
 		win.addstr(title)
 
 		win.setpos(0,width-12)
 		win.addstr("Time: " + time_offset.to_s)
-
-		win.setpos(height-1,3)
-		win.addstr("Speed: " + display_speed.to_s)
-
-		win.setpos(height-1,width-12)
-		win.addstr("Score: " + (game_score-(time_offset)/10.round(0)).to_s)
 
 		Snake::draw_snake
 		GamePlay::change_direction_detect
@@ -171,7 +167,7 @@ begin
 		GamePlay::change_direction_update
 		Snake::check_wall_collision
 		Snake::check_self_collision
-		Snake::check_food_eaten
+		Snake::check_food_eaten(time_offset)
 		GamePlay::proper_delay
 
 		win.refresh
